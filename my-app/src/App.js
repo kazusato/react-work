97e0175a145782c69be6325525867811301bf01a
sato <super80@gmail.com>
2018-04-15 17:18:57 +0900

    JSXをいろいろな所で使う

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,45 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// JSX内に式を埋め込む
 const fullnames = {
   ryoma: '坂本龍馬',
   taka: '西郷隆盛',
   kai: '勝海舟',
 };
 const getFullname = nickname => fullnames[nickname];
+
+// 関数の戻り値にJSXを使う
+function Hello(name) {
+  if (typeof name === 'string') {
+    return <span>Hello, {name}</span>;
+  }
+  return <span>Who are you?</span>;
+}
+
+// for内でJSXを利用
+function Names(names) {
+  const list = [];
+  for (let i=0; i<names.length; i++) {
+    list.push(<li>{Hello(names[i])}</li>);
+  }
+  return <ul>{list}</ul>;
+}
+
+function wrap(component) {
+  return <div>{component}</div>
+}
+
+const targetList = [
+  "田中　一郎",
+  "鈴木　二郎",
+  3,
+  "木村　四郎",
+];
+
+// 関数の引数
+const nameListTitle = wrap(<h1>名前一覧</h1>);
+const nameList = wrap(Names(targetList));
 
 class App extends Component {
   render() {
@@ -20,6 +53,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {nameListTitle}
+        {nameList}
       </div>
     );
   }
