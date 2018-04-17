5e4d70052236e869507cb0c283c637fdbc0c947a
Kazuhiko Sato <kazuhiko.sato@unisys.co.jp>
2018-04-17 17:42:40 +0900

    todo-app初期段階

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,43 @@ import React, {Component} from 'react';
 
 const FuncHello = (props) => {
     return <div>こんにちは、{props.name}さん。</div>;
-}
+};
 
 class ClassHello extends Component {
     render() {
         return <div>こんにちは、{this.props.name}様。</div>;
     }
 }
+
+const HelloFragment = () => {
+    return (
+        <React.Fragment>
+            <FuncHello name="おおお"/>
+            <ClassHello name="かかか"/>
+        </React.Fragment>
+    );
+};
+
+const DefaultPropsHello = (props) => {
+    return (
+        <div>こんにちは、{props.name}さん。</div>
+    );
+};
+DefaultPropsHello.defaultProps = {
+    name: '名無し'
+};
+
+// 手元環境では実行時にSyntax errorになった。
+/*
+const ShortFragment = (name) => {
+    return (
+        <>
+            <FuncHello name="ききき"/>
+            <ClassHello name="くくく"/>
+        </>
+    );
+};
+*/
 
 class App extends Component {
     render() {
@@ -18,6 +48,9 @@ class App extends Component {
                 <ClassHello name="いいい"/>
                 <ClassHello name="ううう"/>
                 <ClassHello name="えええ"/>
+                <HelloFragment/>
+                <DefaultPropsHello name="さささ"/>
+                <DefaultPropsHello/>
             </div>
         );
     }
