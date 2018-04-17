3d0dfbee0cf010c090fe1c8dfc5cf37fdf7c7c06
sato <super80@gmail.com>
2018-04-18 04:37:16 +0900

    Reducer/Store/Actionを作り動かすところまで完了

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const initialState = {
+    tasks: [],
+};
+
+function tasksReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'ADD_TASK':
+            return {
+                ...state,
+                tasks: state.tasks.concat([action.payload.task]),
+            };
+        default:
+            return state;
+    }
+}
+
+const store = createStore(tasksReducer);
+
+const addTask = (task) => ({
+    type: 'ADD_TASK',
+    payload: {
+        task
+    }
+});
+
+console.log(store.getState());
+store.dispatch(addTask('Storeを学ぶ'));
+console.log(store.getState());
